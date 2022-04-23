94aeeec074a7e13764f9e6b620ab4bb395944f75
EthanPark <ethanpark604@gmail.com>
2022-04-23 14:22:18 +0900

    [Add] ViewRepresentable Protocol

---
+++ GithubSearchApp/Util/Protocol/ViewRepresentable.swift
@@ -0,0 +1,13 @@
+//
+//  ViewRepresentable.swift
+//  GithubSearchApp
+//
+//  Created by 박근보 on 2022/04/23.
+//
+
+import Foundation
+
+protocol ViewRepresentable {
+    func setUpView()
+    func setUpConstraint()
+}
