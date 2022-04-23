7119045d1c65eda5acdade99f7a6a9b5e4e26339
EthanPark <ethanpark604@gmail.com>
2022-04-24 03:23:48 +0900

    [Add] SearchUser Logic(SearchViewController, SearchViewModel)

--- GithubSearchApp/View/SearchViewController.swift
+++ GithubSearchApp/View/SearchViewController.swift
@@ -6,14 +6,88 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class SearchViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    private lazy var input = SearchViewModel.Input(
+        requestUserListEvent: requestUserListEvent.asSignal(),
+        requestNextPageListEvent: requestNextPageListEvent.asSignal(),
+        searchFavoriteUserListEvent: searchFavoriteUserListEvent.asSignal(),
+        pressFavoriteButtonEvent: pressFavoriteButtonEvent.asSignal()
+    )
+    private lazy var output = viewModel.transform(input: input)
+
+    private let requestUserListEvent = PublishRelay<String>()
+    private let requestNextPageListEvent = PublishRelay<String>()
+    private let searchFavoriteUserListEvent = PublishRelay<String>()
+    private let pressFavoriteButtonEvent = PublishRelay<Int>()
+
+    private let mainView = SearchView()
+    private var viewModel = SearchViewModel()
+    private let disposeBag = DisposeBag()
+
+    override func loadView() {
+        self.view = mainView
     }
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
+        view.backgroundColor = .white
+
+        navigationItemConfig()
+        setViewConfig()
+        bind()
+    }
+
+    private func navigationItemConfig() {
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: mainView.titleLabel)
+    }
+
+    private func setViewConfig() {
+
+        mainView.apiButton.tapButton.addTarget(self, action: #selector(apiButtonTap), for: .touchUpInside)
+        mainView.localButton.tapButton.addTarget(self, action: #selector(localButtonTap), for: .touchUpInside)
+
+        mainView.searchBar.delegate = self
+        mainView.searchBar.searchTextField.delegate = self
+
+        mainView.searchTableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
+        mainView.searchTableView.rowHeight = 100
+    }
+
+    private func bind() {
+
+        output.didLoadUserList
+            .drive(mainView.searchTableView.rx.items(cellIdentifier: SearchTableViewCell.identifier, cellType: SearchTableViewCell.self)) { (row, element, cell) in
+                cell.cellConfig(searchItem: element)
+            }
+            .disposed(by: disposeBag)
+
+    }
+
+    @objc private func apiButtonTap() {
+        guard mainView.apiButton.status == .deselected else { return }
+
+        mainView.apiButton.status = .selected
+        mainView.localButton.status = .deselected
+    }
+
+    @objc private func localButtonTap() {
+        guard mainView.localButton.status == .deselected else { return }
+
+        mainView.apiButton.status = .deselected
+        mainView.localButton.status = .selected
+    }
 }
 
+extension SearchViewController: UISearchBarDelegate, UISearchTextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        guard let query = mainView.searchBar.searchTextField.text else { return true }
+        requestUserListEvent.accept(query)
+        textField.resignFirstResponder()
+        return true
+    }
+}
--- GithubSearchApp/ViewModel/SearchViewModel.swift
+++ GithubSearchApp/ViewModel/SearchViewModel.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
+
+final class SearchViewModel: ViewModelType {
+
+    struct Input {
+        // API Tab
+        let requestUserListEvent: Signal<String>
+        let requestNextPageListEvent: Signal<String>
+        // Local Tab
+        let searchFavoriteUserListEvent: Signal<String>
+        // common
+        let pressFavoriteButtonEvent: Signal<Int>
+    }
+
+    struct Output {
+        // API Tab
+        let didLoadUserList: Driver<[SearchItem]>
+        // Local Tab
+        let didLoadLocalUserList: Driver<[SearchItem]>
+        // Common
+        let didPressFavoriteButton: Signal<Int>
+        let failToastAction: Signal<String>
+        let indicatorActin: Driver<Bool>
+    }
+
+    // API Tab
+    private let didLoadUserList = BehaviorRelay<[SearchItem]>(value: [])
+    // Local Tab
+    private let didLoadLocalUserList = BehaviorRelay<[SearchItem]>(value: [])
+    // Common
+    private let didPressFavoriteButton = PublishRelay<Int>()
+    private let failToastAction = PublishRelay<String>()
+    private let indicatorAction = BehaviorRelay<Bool>(value: false)
+
+    var disposeBag = DisposeBag()
+
+    var totalSearchItem: [SearchItem] = []
+
+    func transform(input: Input) -> Output {
+
+        input.requestUserListEvent
+            .emit { [weak self] query in
+                guard let self = self else { return }
+                self.totalSearchItem.removeAll()
+                self.requestSearchUser(query: query) { response in
+                    switch response {
+                    case .success(let data):
+                        self.appendData(searchItem: data.searchItems)
+                        self.didLoadUserList.accept(self.totalSearchItem)
+                    case .failure(let error):
+                        self.failToastAction.accept(error.errorDescription ?? "Error")
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        return Output(
+            didLoadUserList: didLoadUserList.asDriver(),
+            didLoadLocalUserList: didLoadLocalUserList.asDriver(),
+            didPressFavoriteButton: didPressFavoriteButton.asSignal(),
+            failToastAction: failToastAction.asSignal(),
+            indicatorActin: indicatorAction.asDriver()
+        )
+    }
+}
+
+extension SearchViewModel {
+
+    private func requestSearchUser(query: String, completion: @escaping (Result<SearchData, SearchError>) -> Void) {
+
+        let parameter = [
+            "q": "\(query)"
+        ]
+
+        APIManager.shared.requestSearchUser(parameter: parameter, completion: completion)
+    }
+
+    private func appendData(searchItem: [SearchItem]) {
+        for i in searchItem {
+            totalSearchItem.append(i)
+        }
+    }
+}
