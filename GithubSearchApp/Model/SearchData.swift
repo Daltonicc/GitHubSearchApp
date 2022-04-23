dc8ec55d5e32ea733dc82d805cf37230ed8b2379
EthanPark <ethanpark604@gmail.com>
2022-04-24 00:45:37 +0900

    [Add] NetworkError

---
+++ GithubSearchApp/Model/SearchData.swift
@@ -0,0 +1,8 @@
+//
+//  SearchData.swift
+//  GithubSearchApp
+//
+//  Created by 박근보 on 2022/04/24.
+//
+
+import Foundation
---
+++ GithubSearchApp/Network/NetworkError.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkError.swift
+//  GithubSearchApp
+//
+//  Created by 박근보 on 2022/04/24.
+//
+
+import Foundation
+
+enum SearchError: Int, Error {
+    case validationFailed = 422
+    case serviceError = 503
+}
+
+extension SearchError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .validationFailed: return "검색이 유효하지 않습니다"
+        case .serviceError: return "서버 오류로 시도할 수 없습니다"
+        }
+    }
+}
