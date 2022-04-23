ba59c82eb2db0cf84a4052d49c98291a3a431739
EthanPark <ethanpark604@gmail.com>
2022-04-24 00:46:48 +0900

    [Add] APIService

--- GithubSearchApp/Network/APIService.swift
+++ GithubSearchApp/Network/APIService.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+import Moya
+
+enum GitHubSearchAPI {
+    case searchUser(query: String)
+}
+
+extension GitHubSearchAPI: TargetType {
+
+    var baseURL: URL {
+        return URL(string: APIKey.baseURL)!
+    }
+
+    var path: String {
+        switch self {
+        case .searchUser(let query): return "/search/users?q=\(query)"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .searchUser: return .get
+        }
+    }
+
+    var task: Task {
+        switch self {
+        case .searchUser: return .requestPlain
+        }
+    }
+
+    var headers: [String : String]? {
+        switch self {
+        case .searchUser: return [
+            "Accept": "application/vnd.github.v3+json",
+            "Authorization": APIKey.authorization
+        ]
+        }
+    }
+}
