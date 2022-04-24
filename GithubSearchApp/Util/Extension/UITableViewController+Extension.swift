40e99d1b04a2474af5f5a6578a21d195d2b0662b
EthanPark <ethanpark604@gmail.com>
2022-04-24 13:20:44 +0900

    [Add] Press Animation

--- GithubSearchApp/Util/Extension/UITableViewController+Extension.swift
+++ GithubSearchApp/Util/Extension/UITableViewController+Extension.swift
@@ -11,4 +11,20 @@ extension UITableViewCell {
     static var identifier: String {
         return String(describing: self)
     }
+
+    func addPressAnimationToButton(scale: Double, _ viewToAnimate: UIView, completion: ((Bool) -> Void)?) {
+
+        UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+
+            viewToAnimate.transform = CGAffineTransform(scaleX: scale, y: scale)
+
+        }) { _ in
+
+            UIView.animate(withDuration: 0.15, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 2, options: .curveEaseIn, animations: {
+
+                viewToAnimate.transform = CGAffineTransform(scaleX: 1, y: 1)
+
+            }, completion: completion)
+        }
+    }
 }
