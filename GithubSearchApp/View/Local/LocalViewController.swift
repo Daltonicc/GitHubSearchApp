4f18f3ead6cf7654a965b709c71ff0ad4dd39918
EthanPark <ethanpark604@gmail.com>
2022-04-27 20:12:57 +0900

    [Add] didLoadFavoriteUserList Event

--- GithubSearchApp/View/Local/LocalViewController.swift
+++ GithubSearchApp/View/Local/LocalViewController.swift
@@ -6,13 +6,34 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 final class LocalViewController: BaseViewController {
 
+    private lazy var input = LocalViewModel.Input(
+        requestFavoriteUserListEvent: requestFavoriteUserListEvent.asSignal(),
+        searchFavoriteUserListEvent: searchFavoriteUserListEvent.asSignal(),
+        pressFavoriteButtonEvent: pressFavoriteButtonEvent.asSignal()
+    )
+    private lazy var output = viewModel.transform(input: input)
+
+    private let requestFavoriteUserListEvent = PublishRelay<Void>()
+    private let searchFavoriteUserListEvent = PublishRelay<String>()
+    private let pressFavoriteButtonEvent = PublishRelay<Int>()
+
     private let mainView = ContentView()
+    private var viewModel = LocalViewModel()
+    private let disposeBag = DisposeBag()
+
+    private var favoriteUserList: [UserItem] = []
 
     override func loadView() {
         self.view = mainView
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        requestFavoriteUserListEvent.accept(())
     }
 
     override func viewDidLoad() {
@@ -21,6 +42,8 @@ final class LocalViewController: BaseViewController {
 
     override func setViewConfig() {
         super.setViewConfig()
+
+        mainView.noResultView.status = .local
         
         mainView.searchBar.delegate = self
         mainView.searchBar.searchTextField.delegate = self
@@ -28,9 +51,26 @@ final class LocalViewController: BaseViewController {
         mainView.searchTableView.register(SearchTableViewCell.self, forCellReuseIdentifier: SearchTableViewCell.identifier)
         mainView.searchTableView.keyboardDismissMode = .onDrag
         mainView.searchTableView.rowHeight = 100
+        mainView.searchTableView.delegate = self
+        mainView.searchTableView.dataSource = self
     }
 
     override func bind() {
+
+        output.didLoadFavoriteUserList
+            .drive { [weak self] item in
+                guard let self = self else { return }
+                self.favoriteUserList = item
+                self.mainView.searchTableView.reloadData()
+            }
+            .disposed(by: disposeBag)
+
+        output.noResultAction
+            .drive { [weak self] bool in
+                guard let self = self else { return }
+                self.mainView.noResultView.isHidden = bool
+            }
+            .disposed(by: disposeBag)
 
     }
 }
@@ -45,5 +85,20 @@ extension LocalViewController: UISearchBarDelegate, UISearchTextFieldDelegate {
 extension LocalViewController: SearchTableViewCellDelegate {
     func didTapFavoriteButton(row: Int) {
 
+    }
+}
+
+extension LocalViewController: UITableViewDelegate, UITableViewDataSource {
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoriteUserList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: SearchTableViewCell.identifier, for: indexPath) as? SearchTableViewCell else { return UITableViewCell() }
+        let row = indexPath.row
+        cell.cellConfig(searchItem: favoriteUserList[row], row: row)
+        cell.delegate = self
+        return cell
     }
 }
