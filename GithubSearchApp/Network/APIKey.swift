fa30bf85311ad8e813d13c0a8d0abccfe7fbc41a
EthanPark <ethanpark604@gmail.com>
2022-04-28 21:51:00 +0900

    [Add] APIKey File Without Token

---
+++ GithubSearchApp/Network/APIKey.swift
@@ -0,0 +1,15 @@
+//
+//  APIKey.swift
+//  GithubSearchApp
+//
+//  Created by 박근보 on 2022/04/24.
+//
+
+import Foundation
+
+final class APIKey {
+    static let baseURL = "https://api.github.com"
+    static let authorization = "token \(token)"
+    static let token = "토큰 값을 넣어주세요"
+}
+
